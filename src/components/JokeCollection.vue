0243a8a4c0398eaf47ac951535024dfa7cfcb554
OdysseasKarastergiou <odysseasheat@hotmail.gr>
2025-04-16 13:02:53 +0300

    added sorting of joke collection by rating and alphabetically

--- src/components/JokeCollection.vue
+++ src/components/JokeCollection.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="p-6">
     <h2 class="text-xl mb-4">Your Joke Collection in local storage</h2>
-    <div class="mb-4">
+    <div class="flex justify-between">
+      <div class="mb-4">
       <label for="rating-filter" class="mr-2">Filter by rating:</label>
-      <select id="rating-filter" v-model="ratingFilter" class="border rounded p-2">
+      <font-awesome-icon class="mr-2" :icon="['fas','filter']"/>
+      <select id="rating-filter" v-model="ratingFilter" class="border rounded">
         <option value="">All</option>
         <option value="1">1 Star</option>
         <option value="2">2 Stars</option>
@@ -11,6 +13,19 @@
         <option value="4">4 Stars</option>
         <option value="5">5 Stars</option>
       </select>
+      </div>
+      <div class="flex gap-4">
+        <span>Sort By:</span>
+        <div class="flex cursor-pointer" @click="SortBy('rating')">
+          <font-awesome-icon class="mr-1" :icon="['fas','sort-down']" />
+          <span>Rating</span>
+        </div>
+        <div class="flex cursor-pointer" @click="SortBy('alphabetically')">
+          <font-awesome-icon class="mr-1" :icon="['fas','sort-down']" />
+          <span>Alphabetically</span>
+        </div>
+ 
+      </div>
     </div>
     <div v-if="filteredJokes.length === 0">No jokes saved yet.</div>
     <div v-for="joke in filteredJokes" :key="joke.id" class="mb-4">
@@ -27,6 +42,7 @@ import JokeButton from './JokeButton.vue';
 
 const jokes = ref([]);
 const ratingFilter = ref('');
+const sortBy = ref(null);
 
 function loadJokes() {
   jokes.value = JSON.parse(localStorage.getItem('jokes') || '[]');
@@ -35,14 +51,27 @@ function loadJokes() {
 onMounted(loadJokes);
 
 const filteredJokes = computed(() => {
-  if (ratingFilter.value === '') {
-    return jokes.value;
+  let result = [...jokes.value];
+
+  if (ratingFilter.value !== '') {
+    result = result.filter(joke => joke.rating === parseInt(ratingFilter.value));
   }
-  return jokes.value.filter(joke => joke.rating === parseInt(ratingFilter.value));
+
+  if (sortBy.value === 'rating') {
+    result.sort((a, b) => b.rating - a.rating);
+  } else if (sortBy.value === 'alphabetically') {
+    result.sort();
+  }
+
+  return result;
 });
 
 function removeJoke(id) {
   jokes.value = jokes.value.filter(j => j.id !== id);
   localStorage.setItem('jokes', JSON.stringify(jokes.value));
+}
+
+function SortBy(type) {
+  sortBy.value = type;
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import './assets/index.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faSortDown, faFilter } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faStar)
+library.add(faStar, faSortDown, faFilter)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
